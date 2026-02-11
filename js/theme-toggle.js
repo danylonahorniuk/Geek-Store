4cca04c25c08186d9c70b4d98216c47f356c092b
Danya-N <danyakenobi@gmail.com>
2026-02-11 13:22:25 +0200

    і ще нові маленькі покращення

--- js/theme-toggle.js
+++ js/theme-toggle.js
@@ -1,12 +1,6 @@
-// Перемикач теми
 const themeToggle = document.querySelector('.theme-toggle');
+
 themeToggle.addEventListener('click', () => {
   document.body.classList.toggle('dark');
-
-  // Зміна іконки
-  if (document.body.classList.contains('dark')) {
-    themeToggle.textContent = '☀️'; // Сонце
-  } else {
-    themeToggle.textContent = '🌙'; // Місяць
-  }
 });
+
