b857005dd66c68303a44b4a27b64935a3bbe24aa
Danya-N <danyakenobi@gmail.com>
2026-03-09 12:56:42 +0200

    малі покращення банерів на головній

--- js/banners.js
+++ js/banners.js
@@ -1,12 +1,21 @@
 const slides = document.querySelectorAll('.slide');
+const dots = document.querySelectorAll('.dot');
 const counter = document.querySelector('.counter');
+
 let index = 0;
 
-function showSlide(i) {
+function showSlide(i){
+
   slides.forEach(s => s.classList.remove('active'));
+  dots.forEach(d => d.classList.remove('active'));
+
   slides[i].classList.add('active');
-  counter.textContent = `${i + 1} / ${slides.length}`;
+  dots[i].classList.add('active');
+
+  counter.textContent = `${i+1} / ${slides.length}`;
 }
+
+/* arrows */
 
 document.querySelector('.right').onclick = () => {
   index = (index + 1) % slides.length;
@@ -18,7 +27,18 @@ document.querySelector('.left').onclick = () => {
   showSlide(index);
 };
 
-setInterval(() => {
+/* dots click */
+
+dots.forEach((dot,i)=>{
+  dot.onclick = ()=>{
+    index = i;
+    showSlide(index);
+  }
+});
+
+/* autoplay */
+
+setInterval(()=>{
   index = (index + 1) % slides.length;
   showSlide(index);
-}, 6000);
+},6000);
